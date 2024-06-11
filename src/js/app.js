2f5723db22cd4e9f027c83f81cd3b4b5e95da3e9
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-06-11 13:26:35 -0600

    Slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,13 @@
 const sliderContent = document.querySelector ('.work__content');
+const sliderBtnLeft = document.querySelector ('.work__btn--left');
+const sliderBtnRight = document.querySelector ('.work__btn--right');
 
 let sliderIndex = 2;
 const sliderLength = 5;
+
+
+sliderBtnLeft.addEventListener ('click', slidePrev);
+sliderBtnRight.addEventListener ('click', slideNext);
 
 function getIndex (index)
 {
@@ -17,6 +23,42 @@ function getImgUrl (index)
 {
     return `./assets/image-slide-${index + 1}.jpg`;
 }
+
+function slidePrev ()
+{
+    if(sliderContent.classList.contains ('slide-to-left') || sliderContent.classList.contains ('slide-to-right'))
+        return;
+
+    sliderIndex = getIndex (sliderIndex - 1);
+
+    sliderContent.innerHTML = `
+    <img src='${getImgUrl (getIndex (sliderIndex - 1))}' alt='Slider image' class='work__img'/>` + sliderContent.innerHTML;
+
+    sliderContent.classList.add ('work__content--slide-to-left');
+    setTimeout (() => {
+        renderSlider ();
+        sliderContent.classList.remove ('work__content--slide-to-left');
+        
+    }, 500);
+}
+
+function slideNext ()
+{
+    if(sliderContent.classList.contains ('slide-to-left') || sliderContent.classList.contains ('slide-to-right'))
+        return;
+
+    sliderIndex = getIndex (sliderIndex + 1);
+
+    sliderContent.innerHTML = sliderContent.innerHTML + `<img src='${getImgUrl (getIndex (sliderIndex + 1))}' alt='Slider image' class='work__img'/>`;
+
+    sliderContent.classList.add ('work__content--slide-to-right');
+    setTimeout (() => {
+        renderSlider ();
+        sliderContent.classList.remove ('work__content--slide-to-right');
+        
+    }, 500);
+}
+
 renderSlider ();
 
 function renderSlider ()
